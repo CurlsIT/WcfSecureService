baa83c294468bca5d4be2819008fa38081a7c21e
Wiktor Górka <wiktor.gorka.90@gmail.com>
2016-05-01 15:59:42 +0200

    Working SSL with client certificate.

--- WcfSecureService/Program.cs
+++ WcfSecureService/Program.cs
@@ -7,13 +7,13 @@ namespace WcfSecureService
     {
         static void Main(string[] args)
         {
-            var serviceHost = new ServiceHost(typeof(SampleService));
-            serviceHost.Open();
+            using (var serviceHost = new ServiceHost(typeof(SampleService)))
+            {
+                serviceHost.Open();
 
-            Console.WriteLine("Service started. Press [Enter] to exit.");
-            Console.ReadLine();
-
-            serviceHost.Close();
+                Console.WriteLine("Service started. Press [Enter] to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
